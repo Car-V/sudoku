4e0bc79a2b58b5a5b17c710cdfc4324a3564f867
Car-V <vale.c@northeastern.edu>
2022-04-21 11:30:01 -0400

    more clues until solution number is resolved

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import numpy
 
 # maybe can make a function for the random.sample
 
-# changing rows and cols to being from 0 to 8 fixed issues,
-# but now I need to take into account the 3x3 boxes
-
 sudoku = []
 solution = []
 
@@ -53,33 +50,36 @@ def allowed(hor, vert, symbol, sud):
     return check_box(hor, vert, symbol, sud)
 
 
-def solve(c, d, cells, count):
+# this method needs reviewing -- give sudoku more clues
+def solve(c, d, boxes, count):
     if c == 9:
         c = 0
         if d + 1 == 9:
             return count + 1
-    if cells[c][d] != 0:  # skip filled cells
-        return solve(c+1, d, cells, count)
+    # to skip the filled boxes
+    if boxes[c][d] != 0:
+        return solve(c+1, d, boxes, count)
+    # checking to be placed
     for x in range(1, 10):
-        if allowed(c, d, x, cells) and count < 2:
-            cells[c][d] = x
-            count = solve(c+1, d, cells, count)
-    cells[c][d] = 0  # rest on backtrack
+        if allowed(c, d, x, boxes) and count < 2:
+            boxes[c][d] = x
+            count = solve(c+1, d, boxes, count)
+    boxes[c][d] = 0
     return count
 
 
 number_of_solutions = 2
 while number_of_solutions != 1:
-    # fixed by using
+
     # random.sample(range(3), 3) provides a random array with values [0, 1, 2]
-    #
+
     # like with rows and cols being used to make unique vals, these ranges and the operation create unique values for
     # appending to rows from 0 to 8, and do so in such a manner that 3x3 boxes are outlined within the entire board,
     # the board being outlined by the whole of rows and cols ([0 1 2] versus [0 1 2 3 4 5 6 7 8])
 
     # these rows and columns are abstract; segment, made from appended vals, made from a unique combination derived from
-    # rows and cols, is each actual row, and cols don't have matching nums because of the unique way vals is found by both
-    # 'rows' and 'cols'
+    # rows and cols, is each actual row, and cols don't have matching nums because of the
+    # unique way vals is found by both 'rows' and 'cols'
     rows = []
     for i in random.sample(range(3), 3):
         for a in random.sample(range(3), 3):
@@ -98,37 +98,21 @@ while number_of_solutions != 1:
             # highest val from just within parenthesis is 17; (9, 8) makes 9 + 6 + 2, versus (8, 9) makes 8 + 0 + 3
             # (8, 5) versus (7, 6) is 8 + 6 + 1 versus 7 + 0 + 2, though their original sums are equal
             val = (i + 3*(j % 3) + j//3) % 9 + 1
-            # print(val)
             segment.append(val)
-        # print(segment)
-        # print()
         sudoku.append(segment)
         segment = []
-    # print(sudoku)
-
-    # print()
-    # for lin in sudoku:
-    #     print(lin)
 
     # board has now been created; need to extract some values that still allow it to be solved
     # make board look cleaner
 
     # since sudoku is being changed, need copy for solution
     solution = numpy.copy(sudoku)
-    # print()
 
     # 21 is the minimum number of clues needed for a solution, though there could be multiple solutions
+    # usually set to 60 instead of 35
     # unique random values being erased
-    for blank_square in random.sample(range(81), 60):
+    for blank_square in random.sample(range(81), 35):
         sudoku[blank_square % 9][blank_square // 9] = 0
-
-    # not necessary but just to see its current state
-    # for lin in sudoku:
-    #     print('[', end='')
-    #     for n in lin:
-    #         print(f' {n}', end='')
-    #     print(' ]')
-    # print()
 
     number_of_solutions = solve(0, 0, sudoku, 1)
 
