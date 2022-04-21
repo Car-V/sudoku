114eecb52c0646d598b67851f805118751ca4a0f
Car-V <vale.c@northeastern.edu>
2022-04-21 07:10:32 -0400

    some comments cleaned

--- main.py
+++ main.py
@@ -14,31 +14,31 @@ solution = []
 def check_box(h, v, check, sub):
     s = numpy.array(sub)
     if (h, v) == (0, 0) or (0, 3) or (0, 6) or (3, 0) or (3, 3) or (3, 6) or (6, 0) or (6, 3) or (6, 6):   # top left
-        if check in s[h:h+3][v:v+3]: # or s[h+1][v:v+3] or s[h+2][v:v+3]:
+        if check in s[h:h+3][v:v+3]:
             return False
     elif (h, v) == (0, 1) or (0, 4) or (0, 7) or (3, 1) or (3, 4) or (3, 7) or (6, 1) or (6, 4) or (6, 7):   # top mid
-        if check in s[h:h+3][v-1:v+2]: # or s[h+1][v-1:v+2] or s[h+2][v-1:v+2]:
+        if check in s[h:h+3][v-1:v+2]:
             return False
     elif (h, v) == (0, 2) or (0, 5) or (0, 8) or (3, 2) or (3, 5) or (3, 8) or (6, 2) or (6, 5) or (6, 8):   # top right
-        if check in s[h:h+3][v-2:v+1]: # or s[h+1][v-2:v+1] or s[h+2][v-2:v+1]:
+        if check in s[h:h+3][v-2:v+1]:
             return False
     elif (h, v) == (1, 0) or (1, 3) or (1, 6) or (4, 0) or (4, 3) or (4, 6) or (7, 0) or (7, 3) or (7, 6):   # left mid
-        if check in s[h-1:h+2][v:v+3]: # or s[h][v:v+3] or s[h+1][v:v+3]:
+        if check in s[h-1:h+2][v:v+3]:
             return False
     elif (h, v) == (1, 1) or (1, 4) or (1, 7) or (4, 1) or (4, 4) or (4, 7) or (7, 1) or (7, 4) or (7, 7):   # mid
-        if check in s[h-1:h+2][v-1:v+2]: # or s[h][v-1:v+2] or s[h+1][v-1:v+2]:
+        if check in s[h-1:h+2][v-1:v+2]:
             return False
     elif (h, v) == (0, 0) or (0, 3) or (0, 6) or (3, 0) or (3, 3) or (3, 6) or (6, 0) or (6, 3) or (6, 6):   # right mid
-        if check in s[h-1:h+2][v-2:v+1]: # or s[h+1][v:v+3] or s[h+2][v:v+3]:
+        if check in s[h-1:h+2][v-2:v+1]:
             return False
     elif (h, v) == (0, 0) or (0, 3) or (0, 6) or (3, 0) or (3, 3) or (3, 6) or (6, 0) or (6, 3) or (6, 6):   # bottom left
-        if check in s[h-2:h+1][v:v+3]: # or s[h+1][v:v+3] or s[h+2][v:v+3]:
+        if check in s[h-2:h+1][v:v+3]:
             return False
     elif (h, v) == (0, 0) or (0, 3) or (0, 6) or (3, 0) or (3, 3) or (3, 6) or (6, 0) or (6, 3) or (6, 6):   # bottom mid
-        if check in s[h-2:h+1][v-1:v+2]: # or s[h+1][v:v+3] or s[h+2][v:v+3]:
+        if check in s[h-2:h+1][v-1:v+2]:
             return False
     elif (h, v) == (0, 0) or (0, 3) or (0, 6) or (3, 0) or (3, 3) or (3, 6) or (6, 0) or (6, 3) or (6, 6):   # bottom right
-        if check in s[h-2:h+1][v-2:v+1]: # or s[h+1][v:v+3] or s[h+2][v:v+3]:
+        if check in s[h-2:h+1][v-2:v+1]:
             return False
     else:
         return True
@@ -69,17 +69,13 @@ def solve(c, d, cells, count):
 
 
 number_of_solutions = 2
-while (number_of_solutions != 1):
-
-
-
+while number_of_solutions != 1:
     # fixed by using
     # random.sample(range(3), 3) provides a random array with values [0, 1, 2]
     #
     # like with rows and cols being used to make unique vals, these ranges and the operation create unique values for
     # appending to rows from 0 to 8, and do so in such a manner that 3x3 boxes are outlined within the entire board,
     # the board being outlined by the whole of rows and cols ([0 1 2] versus [0 1 2 3 4 5 6 7 8])
-
 
     # these rows and columns are abstract; segment, made from appended vals, made from a unique combination derived from
     # rows and cols, is each actual row, and cols don't have matching nums because of the unique way vals is found by both
@@ -92,8 +88,6 @@ while (number_of_solutions != 1):
     for j in random.sample(range(3), 3):
         for b in random.sample(range(3), 3):
             cols.append(3*b + j)
-
-    # base = random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
 
     segment = []
 
@@ -183,4 +177,3 @@ while com.upper() != 'Y':
     com = input('Press \'y\' to see the solution: ')
 print()
 display(solution)
-# this is assuming that the sudoku we are using has one solution, which we need to confirm
