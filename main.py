acfbec2ebbb8558d9512b602e42d1279bd3b3927
Car-V <vale.c@northeastern.edu>
2022-04-20 23:50:06 -0400

    getting started with rows and columns

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# here is an edit
+# this is the actual file
+import random
+import numpy as np
+
+
+rows = random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
+cols = random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
+print(rows, cols)
+puzzle = []  # need an array 9x9
+#  need to make it so that none of the columns and rows match a number
