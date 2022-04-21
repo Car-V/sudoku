622097450f92454b505e315859b0cf74cc12e729
Car-V <vale.c@northeastern.edu>
2022-04-21 00:36:50 -0400

    attempting to create unique values for board

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import random
 import numpy as np
 
 
+# maybe can make a function for the random.sample
 rows = random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
 cols = random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
-print(rows, cols)
-puzzle = []  # need an array 9x9
+
+base = random.sample([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
+
+for i in rows:
+    for j in cols:
+        # this returns a value from 1 to 9, all of them random and unique before starting again,
+        # and makes it uniquely based on the combination of specific row and specific column (not interchangeable)
+        val = (i + 3*(j % 3) + j//3) % 9 + 1
+        # this will help get a specific index for the array
+        # we can combine this with another random vector (base)
+
+# print(rows, cols)
+# puzzle = []  # need an array 9x9
 #  need to make it so that none of the columns and rows match a number
